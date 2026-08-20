5755edf92aafe8d316e3bc62ea7f6041ed39fce8
Alexandrov Danil <lozzydanil@gmail.com>
2026-08-20 11:29:25 +0300

    change company name

--- addons/website_conditioners/__manifest__.py
+++ addons/website_conditioners/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Website Conditioners",
-    "summary": "Сайт-визитка ПрофиМонтаж: монтаж и электромонтаж",
+    "summary": "Сайт-визитка ТомскМонтаж: монтаж и электромонтаж",
     "version": "18.0.1.2.3",
     "sequence": -999,
     "category": "Website",
